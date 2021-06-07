0023466669af06f19032d4cf5533928ecb6f19c6
Cloud User <centos@ip-172-31-3-163.eu-west-2.compute.internal>
2021-06-07 16:10:43 +0000

    A complete solution to Ex2

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 - Active IoT Devices Monitor
-// Student Name:
-// Date: 
+// Student Name: Adedamilola Tariuwa
+// Date: 07/06/2021
 //
 //  Description: In this exercise, you need to design a counter of active IoT devices, where 
 //  if the rst=1, the counter should be set to zero. If event=0, the value
@@ -19,12 +19,28 @@
 `timescale 1ns / 100ps
 
 module monitor (
-    //Todo: add ports 
+input clk,
+input rst,
+input change,
+input on_off,
+output reg [7:0] counter_out
+);
 
-    );
-                    
-    //Todo: add registers and wires, if needed
+always@(posedge clk)
 
-    //Todo: add user logic
+begin
+
+ if(rst)
+  counter_out<=0;
+ else if(change==0)
+  counter_out<=counter_out;
+ else if (on_off==1)
+   counter_out<=counter_out+1;
+ else 
+   counter_out<=counter_out-1;
+
+end
+
+
       
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Active IoT Devices Monitor
-// Student Name:
-// Date: 
+// Student Name: Adedamilola Tariuwa
+// Date: 07/06/2021
 //
 // Description: A testbench module to test Ex3 - Active IoT Devices Monitor
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -13,16 +13,79 @@
 module top_tb(
     );
     
-//Todo: Parameters
+    //Parameters
+    parameter CLK_PERIOD = 10;
 
-//Todo: Regitsers and wires
+    //Registers and wires
+    reg clk;
+    reg rst;
+    reg change;
+    reg on_off;
+    reg err;
+    reg [7:0] counter_prev;
+    wire [7:0] counter_out;
 
-//Todo: Clock generation
-
-//Todo: User logic
+    //Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
     
-//Todo: Finish test, check for success
+     //Stimulus logic
+     initial begin
+     change=0;
+     on_off=0;
+     counter_prev=0;
+     err=0;
+     rst=1;
 
-//Todo: Instantiate counter module
+     #CLK_PERIOD
+     if (counter_out!=0) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     #CLK_PERIOD
+     rst=0;
+     if (counter_out!=0) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+
+     change=1;
+     forever begin
+     counter_prev= on_off ? counter_prev+1: counter_prev-1;
+     
+     #CLK_PERIOD  
+     if (counter_prev!=counter_out) begin
+      $display("***TEST FAILED!***");
+        err=1;
+     end
+     
+     end
+ 
+     end
+     
+          
+      //Finish simulation and check for success
+      initial begin
+        #50 
+        if (err==0) 
+          $display("***TEST PASSED! :) ***");
+        $finish;
+
+      end
+
+    //User's module
+    monitor top (
+     .rst(rst),
+     .clk(clk),
+     .change (change),
+     .on_off (on_off),
+     .counter_out (counter_out)
+     );
+    
  
 endmodule 
