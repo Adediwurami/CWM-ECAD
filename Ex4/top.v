bde87be965f2dc7f9c655b824285cbf44588890d
Cloud User <centos@ip-172-31-3-163.eu-west-2.compute.internal>
2021-06-08 13:12:47 +0000

    A complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Adedamilola Tariuwa
+// Date: 08/06/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,32 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+
+module lights(clk,rst,button,colour);
+
+input clk,rst,button;
+output colour;
+
+rst=0;
+wire [2:0] colour;
+
+always@(posedge clk)
+ if (colour==3'b000 || colour==3'b111)
+  colour=3'b001;
+ else if (colour==3'b001 && button ==1)
+  colour=3'b010;
+ else if (colour==3'b010 && button ==1)
+  colour=3'b011;
+ else if (colour==3'b011 && button ==1)
+  colour=3'b100;
+ else if (colour==3'b100 && button ==1)
+  colour=3'b101;
+ else if (colour==3'b101 && button ==1)
+  colour=3'b110;
+ else if (colour==3'b110 && button ==1)
+  colour=3'b001;
+ else 
+ colour=colour
+
+
 
