29fb965ddbec8618710696b4aa4478d70e4ee9bc
Cloud User <centos@ip-172-31-3-163.eu-west-2.compute.internal>
2021-06-09 09:38:56 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,98 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+module aircon(clk,temperature,heating,cooling);
+
+input clk,temperature;
+
+[4:0] temperature;
+
+output heating,cooling;
+reg heating, cooling;
+
+reg [1,0] state;
+reg [1,0] next_state;
+
+// The three states
+parameter perfect=2'b00;
+parameter too_cold=2'b01;
+parameter too_hot=2'b10;
+
+// Setting the current and next states
+
+always@(posedge clk) begin
+ if(clk) begin
+   state<=idle;
+   next_state<=perfect;
+ end
+ else begin
+   state<=next_state;
+ end
+end
+
+// Output based on current state
+
+always@(state) begin 
+ case(state)
+  too_hot: begin
+   {heating,cooling}=2'b01;
+  end
+ 
+  case(state)
+  perfect: begin
+   {heating,cooling}=2'b00;
+  end
+  
+  case(state)
+  too_cold: begin
+   {heating,cooling}=2'b10;
+  end
+  
+  default: begin
+ {heating,cooling} = 2'b00;
+  end
+ endcase
+end
+
+// Next state 
+
+always@(state) begin
+ case(state)
+  too_hot: begin
+   next_state = (temperature>5'b10100)?too_hot:perfect;
+  end
+  
+  perfect: begin
+   if (temperature>=5'b10110) begin
+    next_state=too_hot;
+   end
+   
+   else if (temperature<=5'b10010) begin
+    next_state=too_cold;
+   end
+  end 
+
+  too_cold: begin 
+   next_state=(temperature<5'b10100)?too_cold:perfect;
+  end 
+
+  default: begin
+   next_state=perfect;
+  end
+ endcase
+end
+
+endmodule
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,108 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+    //Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Registers and wires
+    reg clk;
+    reg temperature;
+    reg err;
+    wire heating,cooling; 
+
+    //Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+    
+     //Stimulus logic
+     initial begin
+     err=0;
+     
+     // Testing that conditions are met in a clockwise direction from idle
+     
+     temperature=5'b10101;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b00) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     temperature=5'b10110;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b01) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     temperature=5'b10101;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b01) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+
+     temperature=5'b10100;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b00) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     temperature=5'b10001;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b10) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     temperature=5'b10010;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b10) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+
+     temperature=5'b10100;
+
+     #CLK_PERIOD
+     if ({heating,cooling} != 2'b00) begin
+      $display("***TEST FAILED!***");
+      err=1;
+     end
+     
+     end 
+          
+      //Finish simulation and check for success
+      initial begin
+        #100 
+        if (err==0) 
+          $display("***TEST PASSED! :) ***");
+        $finish;
+
+      end
+
+    //User's module
+    aircon top (
+     .clk(clk),
+     .temperature (temperature),
+     .heating(heating),
+     .cooling(cooling)
+     );
+    
+ 
+endmodule 
